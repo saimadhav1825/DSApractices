33fe46a6a4f9cc65d76edacc85371a120e76587a
Sai Madhav Chenna <sai.madhav@applaunch.io>
2022-12-07 21:11:52 +0530

    bubble sort and selection sort

--- src/BinarySearchProblem.java
+++ src/BinarySearchProblem.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class BinarySearchProblem {
     //ascending order array
 
@@ -17,7 +19,7 @@ public class BinarySearchProblem {
         int ceilingOutValue = findCeilingNumber(ceilingArray, ceilingTarget);
         System.out.println(ceilingOutValue);*/
 
-        int[] floorArray = {-10, -1, 0, 1, 5, 7, 9, 10, 17, 19};
+      /*  int[] floorArray = {-10, -1, 0, 1, 5, 7, 9, 10, 17, 19};
         int floorTarget = -5;
         int floorOutValue = floorNumber(floorArray, floorTarget);
         System.out.println(floorOutValue);
@@ -26,7 +28,10 @@ public class BinarySearchProblem {
         char[] charArray = {'a', 'c', 'd', 'k'};
         char charTarget = 'g';
         char charOutValue = findSmallestCharacterGreaterThanTarget(charArray, charTarget);
-        System.out.println(charOutValue);
+        System.out.println(charOutValue);*/
+    //    findFirstLastPositionOfTargetValueInArray();
+        //findIndexOfElement();
+        findPeakElementArray();
     }
 
     private static int ascendingOrderArray(int[] ascendingOrder, int target) {
@@ -133,5 +138,69 @@ public class BinarySearchProblem {
             }
         }
         return arr[start % arr.length];
+    }
+
+    static void findFirstLastPositionOfTargetValueInArray() {
+        int[] ans = {-1, -1};
+        int[] in = {0, 6, 7, 7, 7, 7, 10, 11, 12};
+        int target = 7;
+        int start = search(in, target, true);
+        int end = search(in, target, false);
+        ans[0] = start;
+        ans[1] = end;
+        System.out.println(Arrays.toString(ans));
+    }
+
+    private static int search(int[] in, int target, boolean firstIndex) {
+        int ans = -1;
+        int start = 0;
+        int end = in.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < in[mid]) {
+                end = mid - 1;
+            } else if (target > in[mid]) {
+                start = mid + 1;
+            } else {
+                ans = mid;
+                if (firstIndex) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+
+    //position of element in infinite sorted array
+    static void findIndexOfElement() {
+        int[] arr = {0, 10, 20, 30, 60, 70, 89, 85, 110, 150, 156, 178, 299};
+        int target = 110;
+        int start = 0;
+        int end = 1;
+        while (target > arr[end]) {
+            int temp = end + 1;
+            end = end + (end - start + 1) * 2;
+            start = temp;
+        }
+        System.out.println(ascendingOrderArray(arr, target));
+    }
+
+    static void findPeakElementArray() {
+        int[] arr = {0, 10, 20, 30, 60, 70, 89, 66, 61, 50, 45, 30, 20, 1};
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                //descending
+                end = mid;
+            } else {
+                //ascending
+                start = mid + 1;
+            }
+        }
+        System.out.println(start);
     }
 }
