bb041c729d3838f0ac156c4731f86356e42a09a0
saimadhavchenna <saimadhav1234@gmail.com>
2023-09-08 08:23:20 +0530

    leetcode problems in linear search

--- src/dsalearn/LinearSearchProblems.java
+++ src/dsalearn/LinearSearchProblems.java
@@ -1,14 +1,19 @@
 package dsalearn;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class LinearSearchProblems {
     public static void main(String[] args) {
+        int[][] nums = {{2, -1, 10, 0, -20}, {2, 4, 5, 6, 78}};
+        int[] arr = {12, 345, 2, 6, 7896, 1, 4};
 
-        //Linear Search with Unsorted Array
-        int[] arr = {20, 1, 199,-7, 24, 9, 77, 66};
-        System.out.println("Element Index    " + searchElementIndexIfContain(arr, 1));
-        System.out.println("Element Return If Found   " + searchElementIfFoundElement(arr, 66));
-        System.out.println("If Item Found Or Not  Return Boolean  " + searchElementIfFoundReturnBoolean(arr, 66));
-        System.out.println("If Item Found Or Not  Return Index  " + searchItemWithInIndex(arr, -7, 1, 5));
+        System.out.println("Sum Value "+maximumWealth(nums));
+      //  System.out.println("Max num in the 2D array" + findEvenNumbersInTheArray(arr));
+        // System.out.println("Minimum Num In the Array" + minimumNumInArray(nums));
+        //System.out.println("If Item Found Or Not  Return Index  " + singleNonDuplicate(nums));
+        //  System.out.println("Find Index of the Element in the 2D array" + Arrays.toString(searchElementIn2dArray(nums, 5)));
+        //        System.out.println("Max num in the 2D array" + maxNumIn2dArray(nums));
 
     }
 
@@ -48,5 +53,98 @@ public class LinearSearchProblems {
         if (arr.length == 0) return -1;
         for (int i = start; i <= end; i++) if (arr[i] == item) return i;
         return Integer.MAX_VALUE;
+    }
+    //You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+    //
+    //Return the single element that appears only once.
+    //
+    //Your solution must run in O(log n) time and O(1) space.
+
+    //Example 1:
+    //
+    //Input: nums = [1,1,2,3,3,4,4,8,8]
+    //Output: 2
+
+    private static int singleNonDuplicate(int[] arr) {
+        Arrays.sort(arr);
+        int num = Integer.MAX_VALUE;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i <= arr.length - 1; i++) {
+            if (map.get(i) != null) {
+                if (map.get(i) != arr[i]) {
+                    num = map.get(i);
+                }
+            } else {
+                map.put(0, map.get(0));
+            }
+        }
+        return num;
+    }
+
+    //find the minimum num in the arrau
+    public static int minimumNumInArray(int[] arr) {
+        int ans = arr[0];
+        for (int num : arr) if (num < ans) ans = num;
+        return ans;
+    }
+
+    //Search Element in 2d array
+
+    private static int[] searchElementIn2dArray(int[][] arr, int target) {
+        for (int row = 0; row <= arr.length - 1; row++) {
+            for (int col = 0; col <= arr[row].length - 1; col++) {
+
+                if (arr[row][col] == target) return new int[]{row, col};
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    private static int maxNumIn2dArray(int[][] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int row = 0; row <= arr.length - 1; row++) {
+            for (int col = 0; col <= arr[row].length - 1; col++) {
+                if (arr[row][col] > max) {
+                    max = arr[row][col];
+                }
+            }
+        }
+        return max;
+    }
+
+    //Given an array nums of integers, return how many of them contain an even number of digits.
+    public static int findEvenNumbersInTheArray(int[] nums) {
+        int count = 0;
+        for (int i = 0; i <= nums.length - 1; i++) {
+            if (eventDigits(nums[i])) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static boolean eventDigits(int num) {
+        int count = 0;
+        while (num > 0) {
+            count++;
+            num = num / 10;
+        }
+        return count % 2 == 0;
+    }
+
+
+    //https://leetcode.com/problems/richest-customer-wealth/
+    public static int maximumWealth(int[][] accounts) {
+        int max =0;
+        for (int row = 0; row <= accounts.length - 1; row++) {
+            int sum = 0;
+            for (int col = 0; col <= accounts[row].length - 1; col++) {
+                sum = sum + accounts[row][col];
+            }
+            if (sum >= max) {
+                max = sum;
+            }
+        }
+        return max;
     }
 }
