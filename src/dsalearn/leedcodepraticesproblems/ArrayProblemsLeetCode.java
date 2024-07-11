be4652b6b7499f66f5b42174525bad8c6f2f4b79
saimadhavchenna <saimadhav1234@gmail.com>
2024-07-11 22:01:41 +0530

    recursion

---
+++ src/dsalearn/leedcodepraticesproblems/ArrayProblemsLeetCode.java
@@ -0,0 +1,9 @@
+package dsalearn.leedcodepraticesproblems;
+
+
+public class ArrayProblemsLeetCode {
+    public static void main(String[] args) {
+
+    }
+
+}
--- src/dsalearn/recursion/RecursionClassOneExamples.java
+++ src/dsalearn/recursion/RecursionClassOneExamples.java
@@ -2,8 +2,8 @@ package dsalearn.recursion;
 
 public class RecursionClassOneExamples {
     public static void main(String[] args) {
-        System.out.println(findNumberOfZerosInNumberOtherWya(303030));
-
+        triangle(5, 0);
+        triangle2(5,0);
     }
 
     static void questionOne(int n) {
@@ -96,6 +96,28 @@ public class RecursionClassOneExamples {
             return numberOfStepsHelper(num / 2, c + 1);
         }
         return numberOfStepsHelper(num - 1, c + 1);
+    }
+
+    public static void triangle(int r, int c) {
+        if (r == 0) return;
+        if (c < r) {
+            System.out.print(" * ");
+            triangle(r, c + 1);
+        } else {
+            System.out.println(" ");
+            triangle(r - 1, 0);
+        }
+    }
+
+    public static void triangle2(int r, int c) {
+        if (r == 0) return;
+        if (c < r) {
+            triangle2(r, c + 1);
+            System.out.print(" * ");
+        } else {
+            triangle2(r - 1, 0);
+            System.out.println(" ");
+        }
     }
 
 }
